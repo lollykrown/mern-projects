2fae0386dd9d540234183426170b8d4446c7b83b
lollykrown <joe_kayu@yahoo.com>
2020-10-27 22:06:25 +0100

    start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Header from './Header';
-import Login from './Login';
-import SwipeButtons from './SwipeButtons';
-import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="app">
-      <Login/>
-      {/* <Header/> */}
-      <TinderCards/>
-      <SwipeButtons/>
+
     </div>
   );
 }
