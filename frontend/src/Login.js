491cbb3c8e07308ea6c76033c8c43efba1023a5d
lollykrown <joe_kayu@yahoo.com>
2020-10-27 14:58:23 +0100

    .

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
     return (
         <div className="app">
             <div className="header">
-                <a>
+                <a href="/">
                     <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
                         alt="" /><span className="tinder">tinder</span>
                 </a>
