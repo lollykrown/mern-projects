fd78c9906c51f10fd2db82296f5783599ac758d2
lollykrown <joe_kayu@yahoo.com>
2020-10-29 23:11:16 +0100

    ..

--- frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, applyMiddleware } from "redux";
-import youtubeclone from "./reducers";
-
-export default createStore(
-  youtubeclone,
-  composeWithDevTools(applyMiddleware(thunk))
-);
