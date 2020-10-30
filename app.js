5dd457dc46628821d6462dd8af9aa8f919d6f436
Kay <joe_kayu@yahoo.com>
2020-10-30 08:16:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ db.once('open', function () {
 
 // Set up CORS
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://kay-insta.netlify.app/',
   methods: ['POST', 'PUT', 'GET', 'PATCH','OPTIONS', 'DELETE','HEAD'],
   credentials: true,
   allowedHeaders: "Content-Type, Authorization, X-Requested-With",
