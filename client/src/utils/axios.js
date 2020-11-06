1f06a516c375a1720b9cfad48d8b3bda5da90d52
Kay <joe_kayu@yahoo.com>
2020-11-06 11:22:26 -0800

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,8 +4,8 @@ const CancelToken = axios.CancelToken;
 export const source = CancelToken.source();
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8001',
-    // baseURL: 'https://mern-backend.herokuapp.com',
+//     baseURL: 'http://localhost:8001',
+    baseURL: 'https://mern-backend.herokuapp.com',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Header': '*'
